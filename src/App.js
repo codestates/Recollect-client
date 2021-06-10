422e0655c5b37bd8060f6cf6c71adbb5bb9607db
Siyooon <ksyoon0718@gmail.com>
2021-06-10 16:32:25 +0900

    routingMerge

--- src/App.js
+++ src/App.js
@@ -1,41 +1,52 @@
-import axios from "axios"
-import React from "react"
+import axios from "axios";
+import React from "react";
 // import Landing from "./page/Landing";
 // import Login from "./page/Login";
 // import Signup from "./page/Signup";
 // import Mypage from "./page/Mypage";
 import { Switch, Route, Redirect, withRouter } from "react-router-dom";
 
-require("dotenv").config()
-
-
+require("dotenv").config();
 
 class App extends React.Component {
   constructor(props) {
-    super(props) 
+    super(props);
     this.state = {
       hello: null,
-    }
-    this.getSomething = this.getSomething.bind(this)
+      isLogin: false,
+      username: null,
+    };
+    this.getSomething = this.getSomething.bind(this);
+    this.loginSuccess = this.loginSuccess.bind(this);
   }
-  getSomething() {  
-    console.log(`${process.env.REACT_APP_API_URL}`)
+  getSomething() {
+    console.log(`${process.env.REACT_APP_API_URL}`);
     axios
       .get(`${process.env.REACT_APP_API_URL}`)
-      .then(res => {  
+      .then((res) => {
         this.setState({
-          hello: res.data
-        })
+          hello: res.data,
+        });
       })
-      .catch(err => console.log(err))
+      .catch((err) => console.log(err));
   }
+
+  //// 로그인 성공시 호출메소드 ////
+  loginSuccess = (username) => {
+    this.setState({
+      // 200 응답시 상태변경 //
+      isLogin: true,
+      username: username,
+    });
+    this.props.history.push("/mypage"); // login 성공 => mypage로 라우팅 //
+  };
 
   componentDidMount() {
-    this.getSomething()
+    this.getSomething();
   }
 
-  render() { 
-    return(
+  render() {
+    return (
       <div>
         {/* <Switch>
           <Route
@@ -49,7 +60,7 @@ class App extends React.Component {
           />
           <Route
             path='/login'
-            render={() => (<Login />)}
+            render={() => (<Login loginSuccess = {this.loginSuccess}/>)}
           />
           <Route
             exact
