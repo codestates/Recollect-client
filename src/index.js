45eca1242b77866d96a28b11e9bdb5eccf56e1ef
CodingGorani <tjr2922@gmail.com>
2021-06-14 10:33:27 +0900

    minor changings

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
 
-
-
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/util/randomColor.js
+++ src/util/randomColor.js
@@ -4,17 +4,22 @@ module.exports = {
 
   //사용자가 컬러 theme을 정한다 -> advanced
   // 랜덤테마를 선택했으면 
-  generateRandomColorArr = () => {
+  generateRandomColorPairArr = () => {
     const randomColor = tinycolor.random();
+
     const analogousColor = tinycolor(randomColor).analogous(10, 30);
+
     const analogousColorArr = analogousColor.map(t => {
-      const colorPairObject = { backgorundColor: t.toHexString() }
+      let colorPairObject = { backgorundColor: t.toHexString() }
       if (t.isLight) {
         colorPairObject.textColor = tinycolor('black').toHexString();
       } else {
-        colorPairObject.textColor = tinycolor('black').toHexString();
+        colorPairObject.textColor = tinycolor('white').toHexString();
       }})
     
+      return analogousColorArr;
+
+      //[{backgroundColor: <hexstring>, textColor: <hexstring> // black or white }, {...}, {...}]
 
     
   }
