3267e4b0b852916789d7c186f09a6c9cda24f441
Siyooon <ksyoon0718@gmail.com>
2021-06-15 13:55:29 +0900

    bookmark add#3

--- src/util/randomColor.js
+++ src/util/randomColor.js
@@ -1,20 +1,21 @@
-const tinycolor = require('tinycolor2');
+const tinycolor = require("tinycolor2");
 
 module.exports = {
   //사용자가 컬러 theme을 정한다 -> advanced
   // 랜덤테마를 선택했으면
   generateRandomColorPairArr: () => {
-    const randomColor = tinycolor.random().toHexString();
+    const randomColor = tinycolor.random().toHexString(); // 랜덤컬러 핵사값으로 반환
 
-    const analogousColor = tinycolor(randomColor).analogous(10, 5);
+    const analogousColor = tinycolor(randomColor).analogous(10, 5); // 랜덤컬러 10개 반환,  5?
 
     const analogousColorArr = analogousColor.map((t) => {
       let colorPairObject = { backgroundColor: t.toHexString() };
       if (t.isLight()) {
-        colorPairObject.textColor = tinycolor('black').toHexString();
+        // 배경색과 글작색 조정
+        colorPairObject.textColor = tinycolor("black").toHexString();
         return colorPairObject;
       } else {
-        colorPairObject.textColor = tinycolor('white').toHexString();
+        colorPairObject.textColor = tinycolor("white").toHexString();
         return colorPairObject;
       }
     });
